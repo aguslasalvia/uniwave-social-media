009043e641415abddf6375dfb07b62582a3fd037
aguslasalvia <agus.blumenfeld13@gmail.com>
2026-07-14 15:18:14 -0300

    fix: vercel serverless

--- landing-page/astro.config.mjs
+++ landing-page/astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react"
-import vercel from '@astrojs/vercel/serverless'; // <-- importamos el adapter de Vercel
+import vercel from '@astrojs/vercel'; // <-- importamos el adapter de Vercel
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	integrations: [react()],
-	adapter: vercel({}),
+  output: "server",
+  integrations: [react()],
+  adapter: vercel({}),
 });
